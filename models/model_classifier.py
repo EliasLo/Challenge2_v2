bbfb7166b82cc51c52e48be0c88e89e01f146e1e
EliasLo <146027019+EliasLo@users.noreply.github.com>
2024-04-28 11:21:29 +0200

    Update model_classifier.py

--- models/model_classifier.py
+++ models/model_classifier.py
@@ -143,3 +143,75 @@ class BasicBlock(nn.Module):
         out += self.shortcut(identity)
         out = F.relu(out)
         return out
+
+
+### ResNet_3 with dropout layer
+class ResNet_3(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=50, dropout_rate=0.5):
+        super(ResNet_3, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.dropout = nn.Dropout(dropout_rate)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(self.bn1(x))
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.dropout(x)
+        x = self.fc(x)
+        return x
+
+
+
+
+
+class BasicBlock_2(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1, dropout_rate=0.3):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.dropout1 = nn.Dropout2d(dropout_rate)
+        
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.dropout2 = nn.Dropout2d(dropout_rate)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion * planes)
+            )
+
+    def forward(self, x):
+        identity = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu1(out)
+        out = self.dropout1(out)
+        
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu2(out)
+        out = self.dropout2(out)
+        
+        out += self.shortcut(identity)
+        out = F.relu(out)
+        return out
