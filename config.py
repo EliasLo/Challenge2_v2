baf10537cb7cc6ba62d71de09b15ac416f380b5c
EliasLo <146027019+EliasLo@users.noreply.github.com>
2024-05-06 20:52:02 +0200

    Update config.py

--- config.py
+++ config.py
@@ -35,5 +35,5 @@ patience = 30 # 20 original
 lr = 1e-3
 weight_decay = 1e-3
 warm_epochs = 10
-gamma = 0.900   #0.80 original  #instead 0.96
+gamma = 0.980   #0.80 original  #instead 0.96
 step_size = 5
