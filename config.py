99313ec7431b16fd0683eaca2fadfd266f8e743c
EliasLo <146027019+EliasLo@users.noreply.github.com>
2024-05-06 16:06:22 +0200

    Update config.py

--- config.py
+++ config.py
@@ -28,10 +28,10 @@ sr = 44100
 
 device_id = 0
 batch_size = 32
-num_workers = 20 # 4 # for kaggle
+num_workers = 4 # 4 # for kaggle
 persistent_workers = True
 epochs = 200 #120 #200 original
-patience = 20 # 20 original
+patience = 30 # 20 original
 lr = 1e-3
 weight_decay = 1e-3
 warm_epochs = 10
