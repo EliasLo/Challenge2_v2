c4fd04f79e91fc5f54c27809f87922bddca49880
EliasLo <146027019+EliasLo@users.noreply.github.com>
2024-05-08 09:41:39 +0200

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ n_classes = 50
 folds = 5
 test_folds = [1, 2, 3, 4, 5]
 # ratio to split off from training data
-val_size = .15  # could be changed # 0.2 original # instead 0.25
+val_size = .10  # could be changed # 0.2 original # instead 0.25
 
 
 # model_constructor = "ResNet(block=ResidualBlock, layers=[2, 2, 2, 2], num_classes=config.n_classes)"
