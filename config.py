5830fff2cd584d096ca316c21daf0019b7ecffa1
EliasLo <146027019+EliasLo@users.noreply.github.com>
2024-05-10 09:27:20 +0200

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ disable_bat_pbar = False#True
 # do not change this block
 n_classes = 50
 folds = 5
-test_folds = [2,3]#[1, 2, 3, 4, 5]
+test_folds = [1,2]#[1, 2, 3, 4, 5]
 # ratio to split off from training data
 val_size = .10  # could be changed # 0.2 original # instead 0.25
 
@@ -30,7 +30,7 @@ device_id = 0
 batch_size = 32
 num_workers = 3 # 4 # for kaggle
 persistent_workers = True
-epochs = 250 #120 #200 original
+epochs = 300 #120 #200 original
 patience = 50 # 20 original
 lr = 1e-3
 weight_decay = 1e-3
