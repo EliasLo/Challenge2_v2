4e21fa9d68ced15855528512d0d753f193596a82
EliasLo <146027019+EliasLo@users.noreply.github.com>
2024-05-17 13:44:40 +0200

    Update model_classifier.py

--- models/model_classifier.py
+++ models/model_classifier.py
@@ -81,46 +81,46 @@ class ResidualBlock(nn.Module):
 
 ### ResNet_3 with dropout layer
 class ResNet_3(nn.Module):
-    def __init__(self, block, layers, num_classes=10, dropout_rate=0.2):
+    def __init__(self, block, num_blocks, num_classes=50, dropout_rate=0.125):
         super(ResNet_3, self).__init__()
-        self.inplanes = 64
+        self.in_planes = 64
         self.conv1 = nn.Sequential(
-            nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3),
+            nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False),
             nn.BatchNorm2d(64),
-            nn.ReLU())
+            nn.ReLU(inplace=True)
+        )
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer0 = self._make_layer(block, 64, layers[0], stride=1, dropout_rate=dropout_rate)
-        self.layer1 = self._make_layer(block, 128, layers[1], stride=2, dropout_rate=dropout_rate)
-        self.layer2 = self._make_layer(block, 256, layers[2], stride=2, dropout_rate=dropout_rate)
-        self.layer3 = self._make_layer(block, 512, layers[3], stride=2, dropout_rate=dropout_rate)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1, dropout_rate=dropout_rate)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2, dropout_rate=dropout_rate)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2, dropout_rate=dropout_rate)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2, dropout_rate=dropout_rate)
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Sequential(
             nn.Dropout(dropout_rate),
-            nn.Linear(512, num_classes)
+            nn.Linear(512 * block.expansion, num_classes)
         )
 
-    def _make_layer(self, block, planes, blocks, stride=1, dropout_rate=0.2):
-        downsample = None
-        if stride != 1 or self.inplanes != planes:
-            downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes, kernel_size=1, stride=stride),
-                nn.BatchNorm2d(planes),
-            )
+    def _make_layer(self, block, planes, num_blocks, stride, dropout_rate):
         layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample, dropout_rate))
-        self.inplanes = planes
-        for i in range(1, blocks):
-            layers.append(block(self.inplanes, planes, dropout_rate=dropout_rate))
-
+        downsample = None
+        if stride != 1 or self.in_planes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_planes, planes * block.expansion, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion)
+            )
+        layers.append(block(self.in_planes, planes, stride, downsample, dropout_rate))
+        self.in_planes = planes * block.expansion
+        for _ in range(1, num_blocks):
+            layers.append(block(self.in_planes, planes, dropout_rate=dropout_rate))
         return nn.Sequential(*layers)
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.maxpool(x)
-        x = self.layer0(x)
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
+        x = self.layer4(x)
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
@@ -128,31 +128,32 @@ class ResNet_3(nn.Module):
 
 
 class BasicBlock_2(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1, downsample=None, dropout_rate=0.2):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1, downsample=None, dropout_rate=0.2):
         super(BasicBlock_2, self).__init__()
         self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU())
+            nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False),
+            nn.BatchNorm2d(planes),
+            nn.ReLU(inplace=True)
+        )
         self.dropout1 = nn.Dropout2d(dropout_rate)
         self.conv2 = nn.Sequential(
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(out_channels))
+            nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(planes),
+            nn.ReLU(inplace=True)
+        )
         self.dropout2 = nn.Dropout2d(dropout_rate)
-        self.downsample = downsample
-        self.relu = nn.ReLU()
-        self.out_channels = out_channels
+        self.shortcut = downsample if downsample else nn.Sequential()
 
     def forward(self, x):
-        residual = x
+        identity = self.shortcut(x)
         out = self.conv1(x)
         out = self.dropout1(out)
         out = self.conv2(out)
         out = self.dropout2(out)
-        if self.downsample:
-            residual = self.downsample(x)
-        out += residual
-        out = self.relu(out)
+        out += identity
+        out = F.relu(out, inplace=True)
         return out
     
 
