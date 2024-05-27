832cf333ff929e4c710a2b64c657db61e4a341ba
EliasLo <146027019+EliasLo@users.noreply.github.com>
2024-05-27 09:38:23 +0200

    Update config.py

--- config.py
+++ config.py
@@ -9,12 +9,12 @@ n_classes = 50
 folds = 5
 test_folds = [5]#[1, 2, 3, 4, 5]
 # ratio to split off from training data
-val_size = .20  # could be changed # 0.2 original # instead 0.25
+val_size = .15  # could be changed # 0.2 original # instead 0.25
 
 
 #model_constructor = "ResNet(block=ResidualBlock, layers=[2, 2, 2, 2], num_classes=config.n_classes)"
 #model_constructor = "ResNet(block=ResidualBlock, layers=[3, 4, 6, 3], num_classes=config.n_classes)"
-model_constructor = "ResNet_3(BasicBlock_2, [3, 2, 2, 4])" 
+model_constructor = "ResNet_3(BasicBlock_2, [3, 4, 2, 4])" 
 
 
 # model checkpoints loaded for testing
@@ -27,13 +27,13 @@ test_experiment = 'results/2024-05-22-15-07'
 sr = 44100
 
 device_id = 0
-batch_size = 45
+batch_size = 32
 num_workers = 4 # 4 # for kaggle
 persistent_workers = True
-epochs = 200           #best so far: 200
+epochs = 300           #best so far: 200
 patience = 80          #best so far: 50
 lr = 1e-3              
 weight_decay = 8e-3    #best so far: 1e-3
 warm_epochs = 10
-gamma = 0.96           #0.94 for 79.8 #0.975 79.6
+gamma = 0.975           #0.94 for 79.8 #0.975 79.6
 step_size = 5
